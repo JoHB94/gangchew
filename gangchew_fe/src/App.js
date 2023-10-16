809bd5f2dbdb9220e58e73d5e565e53cf20f915c
Geon.C <chlrjs1324@gmail.com>
2023-10-16 16:41:30 +0900

    라우터 수정 및 멤버 페이지 클라우드 연결

--- gangchew_fe/src/App.js
+++ gangchew_fe/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./main/Header";
 import FundingList from "./funding/FundingList";
 import FundingCreate from "./funding/FundingCreate";
 import Login from "./member/Login";
@@ -12,10 +11,8 @@ import MyActivityDetail from "./mypage/MyActivityDetail";
 import MyActivityPayment from "./mypage/MyActivityPayment";
 import FundingInfo from "./funding/FundingInfo";
 import FundingTest from "./funding/FundingTest";
-import SelectRegistration from "./member/SelectRegistration";
 import RegistrationForm from "./member/RegistrationForm";
 import MainBoard from "./main/MainBoard";
-import BestRankingList from "./component/BestRankingList";
 import SearchList from "./main/SearchList";
 import Layout from "./component/Layout";
 
@@ -23,24 +20,27 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter>
-      <Header/>
       <Routes>
        {/* <Route exact path='/' element={<Home/>}/> */} 
         {/*홈*/}
-        <Route exact path='/main' element={<MainBoard/>}/>
-        <Route exact path='/fundinglist' element={<FundingList/>}/>
-        <Route exact path='/fundingcreate' element={<FundingCreate/>}/>
-        <Route exact path='/fundinginfo' element={<FundingInfo/>}/>
-        <Route exact path='/fundingtest' element={<FundingTest/>}/>
-        <Route exact path='/login' element={<Login/>}/>
-        <Route exact path='/consumercreate' element={<ConsumerCreate/>}/>        
-        <Route exact path='/consumerdetail/:postId' element={<ConsumerDetail/>}/>
-        <Route exact path='/consumerlist' element={<ConsumerList/>}/>
-        <Route exact path='/myactivitydetail' element={<MyActivityDetail/>}/>
-        <Route exact path='/myactivitypayment' element={<MyActivityPayment/>}/>
-        <Route exact path='/selectRegistration' element={<SelectRegistration/>}/>
-        <Route exact path='/registration' element={<RegistrationForm/>}/>
-        <Route exact path='/searchlist' element={<SearchList/>}/>
+          {/* 헤더 없는 페이지 */}
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/registration' element={<RegistrationForm/>}/>
+
+          {/* 헤더를 포함하는 페이지 */}
+        <Route element={<Layout />}>
+          <Route exact path='/main' element={<MainBoard/>}/>
+          <Route exact path='/fundinglist' element={<FundingList/>}/>
+          <Route exact path='/fundingcreate' element={<FundingCreate/>}/>
+          <Route exact path='/fundinginfo' element={<FundingInfo/>}/>
+          <Route exact path='/fundingtest' element={<FundingTest/>}/>
+          <Route exact path='/consumercreate' element={<ConsumerCreate/>}/>        
+          <Route exact path='/consumerdetail/:postId' element={<ConsumerDetail/>}/>
+          <Route exact path='/consumerlist' element={<ConsumerList/>}/>
+          <Route exact path='/myactivitydetail' element={<MyActivityDetail/>}/>
+          <Route exact path='/myactivitypayment' element={<MyActivityPayment/>}/>
+          <Route exact path='/searchlist' element={<SearchList/>}/>
+        </Route>
       </Routes>
     {/* 푸터 컴포넌트 들어갈 부분*/}
     </BrowserRouter>
--- gangchew_fe/src/main/Header.js
+++ gangchew_fe/src/main/Header.js
@@ -10,37 +10,55 @@ import MessageDrop from './MessageDrop';
 import axios from "axios";
 import { getCookie, removeCookie } from "../member/Cookie";
 import { Link } from "react-router-dom";
+import { useEffect } from 'react';
 
-const Header =()=> {
+const Header = () => {
 
-    const [login, setLogin] = useState(false);
+    const [login, setLogin] = useState('');
+    const MyCookie = getCookie("jwtToken");
+    console.log(MyCookie);
+    console.log(login);
 
-    if (window.location.pathname === '/login' || 
-    window.location.pathname === '/selectRegistration' ||
-    window.location.pathname === '/registration') 
-    return null;
-    
+    useEffect(() => { //화면 렌더링시 쿠키를 발급받았다면 로그인 처리된 것으로 인식
+      if(MyCookie === undefined && login === ''){
+        setLogin(false);
+        alert("로그인이 만료되었습니다.");
+        console.log(login);
+      }else if(MyCookie === undefined && login === false) {
+
+      }else if(MyCookie !== undefined) {
+        setLogin(true);
+        console.log(login);
+      }
+    }, [login])
+
+    const loginHandle = () => { // 클릭시 로그인 페이지로 이동(비로그인의 경우)
+      window.location.href = "/login";
+    }
+
+
     const logoutHandle = (event) => {
       const requestUrl = "http://138.2.114.150:9000/authenticate/logout";
       const requestMethod = "POST";
-      const MyCookie = getCookie("jwtToken");
       console.log(MyCookie);
       if(window.confirm("로그아웃 하시겠습니까?")) {
       axios({
         method: requestMethod,
         url: requestUrl,
         headers: {
-          Authorization: `Bearer ${MyCookie}`, // 쿠키 정보를 요청 헤더에 포함
+          Authorization: `Bearer ${MyCookie}`, // 쿠키 정보를 요청 헤더에 포함 -> 서버에서 검증
         },
       })
         .then((response) => {
           console.log("서버 응답 데이터:", response.data);
   
           if (MyCookie != null) {
-            removeCookie("jwtToken"); // 쿠키 브라우저에서 삭제
+            removeCookie("jwtToken"); // 브라우저에서 쿠키 삭제
+            setLogin(true);
+            alert("로그아웃 되었습니다.")
             window.location.href = "/main";
           } else {
-            alert("로그인 시간이 만료되었습니다."); // 이미 쿠키가 만료된 경우
+            alert("로그인이 만료되었습니다."); // 이미 쿠키가 만료된 경우
           }
         })
         .catch((error) => {
@@ -48,6 +66,8 @@ const Header =()=> {
         });
       };
     };
+
+    
 
     return (
         <div>
@@ -59,7 +79,7 @@ const Header =()=> {
                         </div>
                         
                         <ul id='list'>
-                            <li id='container33'>펀딩list</li>
+                            <Link to="/fundinglist"><li id='container33'>펀딩list</li></Link>
                             <li id='container33'>펀딩작성</li>
                             <li id='container33'>요청list</li>
                         </ul>
@@ -70,7 +90,7 @@ const Header =()=> {
                             <SearchBar/>
                         </div>
                         <div id='member'>
-                            <div id='container33'>
+                            <div id='container33' onClick={login? logoutHandle : loginHandle}>
                                 {login?('로그아웃'):('로그인')}
                             </div>
                             <div id='container33'>
